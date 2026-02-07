0d66384074d023e4b8eb195ab34a5d1209df1a1b
Odysseus11111 <2064778350@qq.com>
2026-02-06 23:25:28 -0500

    Correct a bug in ks_func

--- HW2/src/ks-stat.jl
+++ HW2/src/ks-stat.jl
@@ -4,8 +4,8 @@ should receive input from samples X_i's and samples Y_i's.
 function ks_func(X::AbstractVector,Y::AbstractVector)
     n = length(X)
     m =length(Y)
-    labeled_x =[(val,x_val) for val in X]
-    labeled_y =[(val,y_val) for val in y]
+    labeled_x =[(val,"x_val") for val in X]
+    labeled_y =[(val,"y_val") for val in Y]
     combined_val = vcat(labeled_x,labeled_y)
     sort!(combined_val,by =p->p[1])
 
@@ -13,18 +13,25 @@ function ks_func(X::AbstractVector,Y::AbstractVector)
     cdf_y =0.0
     max_diff =0.0
 
-    for (val,source) in combined_val
-        if source == labeled_x
+    total_num = length(combined_val)
+
+    for (i, (val,source)) in enumerate(combined_val)
+        if source == "x_val"
             cdf_x += 1/n
         else
             cdf_y += 1/m
         end
+
+        if i < total_num && combined_val[i+1][1] ==val
+            continue
+        end 
+
         current_diff = abs(cdf_x-cdf_y)
 
         if current_diff > max_diff
-            max_diff = combined_val
+            max_diff = current_diff
         end
     end
-    
+
     return max_diff
 end
