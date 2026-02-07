e1f70b7eba5429ad272da97089c23025f866b287
Odysseus11111 <2064778350@qq.com>
2026-02-06 21:21:26 -0500

    Implementation on Ks-func

--- HW2/src/ks-stat.jl
+++ HW2/src/ks-stat.jl
@@ -7,7 +7,23 @@ function ks_func(X::AbstractVector,Y::AbstractVector)
     labeled_x =[(val,x_val) for val in X]
     labeled_y =[(val,y_val) for val in y]
     combined_val = vcat(labeled_x,labeled_y)
-    sorted_val = sort(val)
-    max_val = 0.0
+    sort!(combined_val,by =p->p[1])
 
+    cdf_x = 0.0
+    cdf_y =0.0
+    max_diff =0.0
+
+    for (val,source) in combined_val
+        if source == labeled_x
+            cdf_x += 1/n
+        else
+            cdf_y += 1/m
+        end
+        current_diff = abs(cdf_x-cdf_y)
+
+        if current_diff > max_diff
+            max_diff = combined_val
+        end
+    end
+    return max_diff
 end
