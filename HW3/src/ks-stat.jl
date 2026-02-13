3c73a1a1cc8f346b05ada88371f753d81ce9a053
Odysseus11111 <2064778350@qq.com>
2026-02-13 12:50:40 -0500

    Updated the src and test for ks_func_2pt

--- HW3/src/ks-stat.jl
+++ HW3/src/ks-stat.jl
@@ -1,6 +1,55 @@
-"""We write a function ks_func which can calculate the KS statistic, the function
-should receive input from samples X_i's and samples Y_i's.
+"""We combined the ks_func_2pt function together with ks_func which will increase the improvement. The new function
+also receive input from samples X_i's and samples Y_i's.
 """
+
+
+# Implementation for the alternatives two-pointer function 
+
+
+function ks_func_2pt(X::AbstractVector,Y::AbstractVector)
+
+    max_diff= 0
+    cdf_x = 0
+    cdf_y = 0
+    n = length(X)
+    m = length(Y)
+    sort_x = sort(X)
+    sort_y =sort(Y)
+    i=1
+    j=1
+
+    while i<=n || j<=m 
+        if i>n
+            current_val = sort_y[j]
+        elseif j>m
+            current_val = sort_x[i]
+        else 
+            current_val = min(sort_x[i],sort_y[j])
+        end
+    
+
+        while i <=n && sort_x[i]== current_val #??current_val_x == sort_x[i]
+            cdf_x +=1/n
+            i+=1
+        end
+
+        while j <=m && sort_y[j]== current_val
+            cdf_y +=1/m
+            j+=1
+        end
+
+        curr_diff = abs(cdf_x-cdf_y)
+        if curr_diff > max_diff
+            max_diff =curr_diff
+        end
+
+    end
+
+    return max_diff
+
+end        
+
+
 function ks_func(X::AbstractVector,Y::AbstractVector)
     n = length(X)
     m =length(Y)
@@ -35,3 +84,6 @@ function ks_func(X::AbstractVector,Y::AbstractVector)
 
     return max_diff
 end
+
+
+const ks_func = ks_func_2pt
