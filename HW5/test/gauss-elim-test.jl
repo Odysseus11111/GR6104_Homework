2223b299c18f1a5058c3c32dfb1fabc93dbcfd2c
Odysseus11111 <2064778350@qq.com>
2026-03-05 20:43:01 -0500

    Add some tests

---
+++ HW5/test/gauss-elim-test.jl
@@ -0,0 +1,57 @@
+# We do the unit test, and check for simle, corner, boundary and bug catching.
+
+# 1.Simple case : We let A = [2 1; 1 3] b = [5;5] the answer is [2 1]
+
+# 2.Boundary case： We use 1*1 matrix, and let A = identity matrix and see whether
+# it works normally
+
+# 3. Corner case: We let A to be contained extreme values , and use a matrix A that
+# contains negative numbers and float to check
+
+# 4. Bug Catch: We let the input A to be a singular matrix [1 2;2 4], b =[3; 6]
+# And see whether we got the error; And we check whether the A and b would be 
+# changed after we manipulate them using backward and forward functions.
+using Test
+using Revise
+includet("../src/gauss-elim.jl")
+
+@testset "Unit Test" begin
+    @testset "Simple Case" begin
+        A = [2 1; 1 3]
+        b = [5;5]
+        x = [2;1]
+        result = gauss_elimi(A,b)
+        @test isapprox(x,result,atol = 1e-10)
+    end
+    @testset "Boundary Case" begin
+        # A = 1*1 matrix
+        A = [1]
+        b = [2]
+        x = [2]
+        result = gauss_elimi(A,b)
+        @test isapprox(x,result,atol = 1e-10)
+        # A =[1 0;0 1] identity matrix
+        A =[1 0;0 1] 
+        b = [2;3]
+        x =[2;3]
+        result = gauss_elimi(A,b)
+        @test isapprox(x,result,atol = 1e-10)
+    end
+    @testset "Corner Case" begin
+        # Let A contains extreme values
+        # 把原本的 1e-12 换成 1.0，避免在第一步消元时产生绝对的 0.0
+        A = [1e12 1.0 2.0; 1.0 1.0 0.0; 1.0 1.0 1.0]
+        b = [1e12, 1.0, 3.0] 
+        x = [1.0, 0.0, 2.0] 
+        result = gauss_elimi(A,b)
+        @test isapprox(x,result,atol = 1e-10)
+        #use a matrix A that contains negative numbers and float 
+        A = [2.5 -1.0 4.0;-3.0 1.5 2.0;1.0 2.0 -1.5]
+        b = [10.5;-7.5;11.0]
+        x =[3.0;5.0;2.0]
+        result = gauss_elimi(A,b)
+        @test isapprox(x,result,atol = 1e-10)
+    end
+end
+
+
