5084f279ba6cc90f1221a9a67b713e4ff3b9317a
Odysseus11111 <2064778350@qq.com>
2026-03-29 12:11:08 -0400

    Complete the whole Newton's Method part

--- HW6/src/src.jl
+++ HW6/src/src.jl
@@ -9,36 +9,41 @@ function link_func(y)
      return 1/(1+exp(-y))
 end
 
-function gradient_func(theta::Vector{Float64},X::Matrix{Float64},y::Vector{Float64})
-    n =length(y)
-    g = zeros(length(theta))
+function predict_prob(theta::Vector{Float64}, X::Matrix{Float64})
+    n = size(X, 1)
+    y_hat = zeros(n)
     for i in 1:n
-        xi = X[i,:]
-        p = link_func(dot(xi,theta))
-        g += xi *(y[i]-p)
+        y_hat[i] = link_func(dot(@view X[i, :], theta))
     end
-    return g
+    return y_hat
 end
+
+function gradient_func(theta::Vector{Float64},X::Matrix{Float64},y::Vector{Float64})
+    y_hat = predict_prob(theta,X)
+    return X' *(y-y_hat)
+end
+
 
 function f_func(theta::Vector{Float64},X::Matrix{Float64},y::Vector{Float64})
     n =length(y)
     val = 0.0
     for i in 1:n
-        xi = X[i,:]
+        xi = @view X[i,:]
         p = link_func(dot(xi,theta))
         val += y[i]*log(p) + (1-y[i])*log(1-p)
     end
     return val
 end
-function backtracking_ascent_gradient(theta::Vector{Float64}, g::Vector{Float64},X::Matrix{Float64}, y::Vector{Float64},alpha::Float64, beta::Float64)
+function backtracking_ascent(theta::Vector{Float64},d::Vector{Float64},X::Matrix{Float64},y::Vector{Float64},alpha::Float64,beta::Float64)
     eta = 1.0
     f_theta = f_func(theta, X, y)
-    while f_func(theta + eta * g, X, y) < f_theta + alpha * eta * dot(g, g)
+    g_theta = gradient_func(theta, X, y)
+
+    while f_func(theta + eta * d, X, y) < f_theta + alpha * eta * dot(g_theta, d)
         eta *= beta
     end
     return eta
 end
-
 function gradient_ascent(y::Vector{Float64}, X::Matrix{Float64}, T_max::Int, alpha::Float64, beta::Float64)
     _,d = size(X)
     theta = zeros(d)
@@ -50,7 +55,7 @@ function gradient_ascent(y::Vector{Float64}, X::Matrix{Float64}, T_max::Int, alp
     for t in 1:T_max
         elapsed = @elapsed begin
             g = gradient_func(theta,X,y)
-            eta = backtracking_ascent_gradient(theta, g, X, y, alpha, beta)
+            eta = backtracking_ascent(theta, g, X, y, alpha, beta)
             theta = theta + eta*g
             etas[t] =eta
             f_vals[t+1] = f_func(theta,X,y)
@@ -60,42 +65,20 @@ function gradient_ascent(y::Vector{Float64}, X::Matrix{Float64}, T_max::Int, alp
     return theta,f_vals,etas,times
 end
 
-
-
 # Dampened Newton’s Method
 #that implements backtracking to select a step size.
 
-function hessian_func(X::Matrix{Float64},theta::Vector{Float64})
-    n,d = size(X)
-    H =zeros(d,d)
-    y_hat = zeros(n)
-    for i in 1:n
-        y_hat[i] = link_func(dot(X[i,:],theta))
-        for k in 1:d
-            for l in 1:d
-                H[k,l] += X[i,k]*y_hat[i]*(1 - y_hat[i])* X[i,l]
-            end
-        end
-    end  
-    return -H   
+function hessian_func(X::Matrix{Float64}, theta::Vector{Float64})
+    y_hat = predict_prob(theta, X)
+    W =Diagonal(y_hat .*(1 .-y_hat))
+    return -(X' * W * X)
 end
-
 
 function newton_dir(X::Matrix{Float64},theta::Vector{Float64},y::Vector{Float64})
     H = hessian_func(X,theta)
     g =gradient_func(theta,X,y)
     return -(H\g)
 end 
-
-function backtracking_ascent_newton(theta::Vector{Float64}, d::Vector{Float64},X::Matrix{Float64}, y::Vector{Float64},alpha::Float64, beta::Float64)
-    eta = 1.0
-    f_theta = f_func(theta, X, y)
-    f_gradient = gradient_func(theta, X,y)
-    while f_func(theta + eta * d, X, y) < f_theta + alpha * eta * dot(f_gradient,d)
-        eta *= beta
-    end
-    return eta
-end
 
 function newton_ascent(y::Vector{Float64}, X::Matrix{Float64}, T_max::Int, alpha::Float64, beta::Float64)
     _,d = size(X)
@@ -108,7 +91,7 @@ function newton_ascent(y::Vector{Float64}, X::Matrix{Float64}, T_max::Int, alpha
     for t in 1:T_max
         elapsed = @elapsed begin
             d_newton =newton_dir(X,theta,y)
-            eta = backtracking_ascent_newton(theta, d_newton, X, y, alpha, beta)
+            eta = backtracking_ascent(theta, d_newton, X, y, alpha, beta)
             theta = theta + eta*d_newton
             etas[t] =eta
             f_vals[t+1] = f_func(theta,X,y)
